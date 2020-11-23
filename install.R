614169e1791edb734b95c2e8326bb05d9c3aea7d
Sateesh <33637490+sateeshperi@users.noreply.github.com>
2020-11-23 13:26:27 -0800

    Create install.R

---
+++ install.R
@@ -0,0 +1,5 @@
+install.packages("pacman")
+
+library(pacman)
+
+pacman::p_load(c("impute", "pcaMethods", "globaltest", "GlobalAncova", "Rgraphviz", "preprocessCore", "genefilter", "SSPA", "sva", "limma", "KEGGgraph", "siggenes","BiocParallel", "MSnbase", "multtest","RBGL","edgeR","fgsea","httr","qs"))
