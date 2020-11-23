939d93ad91a80aacf197b917fefea72806ea6d39
Sateesh <33637490+sateeshperi@users.noreply.github.com>
2020-11-23 13:27:18 -0800

    Update install.R

--- install.R
+++ install.R
@@ -3,3 +3,10 @@ install.packages("pacman")
 library(pacman)
 
 pacman::p_load(c("impute", "pcaMethods", "globaltest", "GlobalAncova", "Rgraphviz", "preprocessCore", "genefilter", "SSPA", "sva", "limma", "KEGGgraph", "siggenes","BiocParallel", "MSnbase", "multtest","RBGL","edgeR","fgsea","httr","qs"))
+
+
+install.packages("devtools")
+library(devtools)
+
+# Step 2: Install MetaboAnalystR with documentation
+devtools::install_github("xia-lab/MetaboAnalystR", build = TRUE, build_vignettes = TRUE, build_manual =T)
